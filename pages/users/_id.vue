8fca1f1af6fb00bdcf0d7f03d5cf96b71e6c59f4
данило <playgroop@ukr.net>
2022-06-28 11:44:39 +0300

    Первое приложение на nuxt

---
+++ pages/users/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Пользователь {{id}}</h1>
+    <nuxt-link to="/users">
+      Назад
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="container">
+  <h1>
+    Пользователи
+  </h1>
+  <nuxt-link to="/users/3">
+    Перейти к етому пользователю
+  </nuxt-link>
+</div>
+
+</template>
